4f4414951e12f6acb0188c00d4834f5abb55f6d6
Justin Jungst <jjungstprogramming@gmail.com>
2017-03-23 13:47:42 -0500

    messages saving to db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,31 @@
 class PagesController < ApplicationController
   def home
+    @message = Message.new
   end
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      respond_to do |format|
+        format.html
+        format.js {render "create"}
+      end
+    else
+      render '/'
+      respond_to do |format|
+        format.html
+        format.js {render "messageErrors.js.erb"}
+      end
+    end
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(
+        :name,
+        :email,
+        :subject,
+        :message
+      )
+    end
 end
